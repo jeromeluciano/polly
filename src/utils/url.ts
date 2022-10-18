3233df123fb352681af5a5a6e06463099fd0fa19
jerome <44360980+jeromeluciano@users.noreply.github.com>
2022-10-19 01:27:36 +0800

    change url

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,6 +1,6 @@
 export function getUrl() {
   if (process.env.NEXTAUTH_URL == "${VERCEL_URL}") {
-    return process.env.NEXT_PUBLIC_VERCEL_URL;
+    return process.env.URL_VERCEL;
   }
   return process.env.NEXTAUTH_URL;
 }
